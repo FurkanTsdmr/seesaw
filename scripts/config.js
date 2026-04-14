68b147458735762a0f00193574b0a4c686785f7b
FurkanTsdmr <furkantsdmr97@gmail.com>
2026-04-14 13:00:26 +0300

    Added:Button CSS | Weight Colors and Seesaw Values

---
+++ scripts/config.js
@@ -0,0 +1,14 @@
+//Weight Colors
+export const COLORS = [
+  "red",
+  "green",
+  "blue",
+  "purple",
+  "aqua",
+  "yellow",
+  "brown",
+  "crimson",
+];
+
+//Seesaw Values
+export const ANGLE = 30;
