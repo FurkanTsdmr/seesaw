10e9f1ffe8325474e988f2d54157d4934cf3671d
FurkanTsdmr <furkantsdmr97@gmail.com>
2026-04-14 11:06:24 +0300

    Query:Click Query Tested -> True

--- scripts/app.js
+++ scripts/app.js
@@ -12,3 +12,14 @@ import {
 } from "./clickable.js";
 // Access Query -> True
 // console.log("Can i access ? ", leftWeight ? "true" : "false");
+// Click Query -> True
+// if (clickArea) {
+//   clickArea.addEventListener("click", (e) => {
+//     console.log(
+//       "Access Click Func.",
+//       e.clientX,
+//       e.clientY,
+//       clickArea ? "true" : "false",
+//     );
+//   });
+// }
