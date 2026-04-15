6672df85a9dd3822d3788a8ddabe5af636824a8d
FurkanTsdmr <furkantsdmr97@gmail.com>
2026-04-15 14:49:49 +0300

    Added:Users Voice Preference

--- scripts/sound.js
+++ scripts/sound.js
@@ -1,4 +1,14 @@
 import { SOUND } from "../scripts/config.js";
+import { muteBtn } from "./clickable.js";
 
 const soundWater = new Audio("../sound/dropsound.mp3");
 soundWater.preload = "auto";
+
+// users' voice preference
+let isMuted = localStorage.getItem(SOUND) === "1";
+
+// Update UI button preference
+const updateButton = (muteBtn) => {
+  muteBtn.setAttribute("aria-pressed", String(isMuted));
+  muteBtn.textContent = isMuted ? "Sound:Off" : "Sound:On";
+};
